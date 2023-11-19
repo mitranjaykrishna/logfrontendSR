c641e4187a1e86eb4ec152c061e5b2c14040aaa8
mitranjaykrishna <mitranjaykrishna@gmail.com>
2023-11-19 13:12:21 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,126 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [formData, setFormData] = useState({
+    level: "",
+    message: "",
+    resourceId: "",
+    timestamp: "",
+    traceId: "",
+    spanId: "",
+    commit: "",
+    parentResourceId: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSearch = async () => {
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/ingest",
+        formData
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error making POST request:", error);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form id="searchForm" className="App">
+        <div></div>
+        <label for="level">Level:</label>
+        <input
+          type="text"
+          id="level"
+          name="level"
+          value={formData.level}
+          onChange={handleChange}
+          required
+        />
+
+        <label for="message">Message:</label>
+        <input
+          type="text"
+          id="message"
+          name="message"
+          value={formData.level}
+          onChange={handleChange}
+          required
+        />
+
+        <label for="resourceId">Resource ID:</label>
+        <input
+          type="text"
+          id="resourceId"
+          name="resourceId"
+          value={formData.level}
+          onChange={handleChange}
+          required
+        />
+
+        <label for="timestamp">Timestamp:</label>
+        <input
+          type="datetime-local"
+          id="timestamp"
+          name="timestamp"
+          value={formData.level}
+          onChange={handleChange}
+          required
+        />
+
+        <label for="traceId">Trace ID:</label>
+        <input
+          type="text"
+          id="traceId"
+          name="traceId"
+          value={formData.level}
+          onChange={handleChange}
+          required
+        />
+
+        <label for="spanId">Span ID:</label>
+        <input
+          type="text"
+          id="spanId"
+          name="spanId"
+          value={formData.level}
+          onChange={handleChange}
+          required
+        />
+
+        <label for="commit">Commit:</label>
+        <input
+          type="text"
+          id="commit"
+          name="commit"
+          value={formData.level}
+          onChange={handleChange}
+          required
+        />
+
+        <label for="parentResourceId">Parent Resource ID:</label>
+        <input
+          type="text"
+          id="parentResourceId"
+          name="parentResourceId"
+          value={formData.level}
+          onChange={handleChange}
+          required
+        />
+
+        <button type="button" onClick={handleSearch}>
+          Search
+        </button>
+      </form>
     </div>
   );
 }
